ef3de7881f89da526295a0e5e7171f01f66bd90a
Bruno Ricci <br9191fr@gmail.com>
2023-01-03 09:48:24 +0100

    Add examples from book

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -19,8 +19,8 @@ fn drink(drink: Option<&str>) {
 
     println!("I love {}s!!!!!", inside);
 }
-
-fn main() {
+#[allow(dead_code)]
+fn act0() {
     let water  = Some("water");
     let lemonade = Some("lemonade");
     //let vide1  = None;
@@ -28,12 +28,106 @@ fn main() {
     give_adult(water);
     give_adult(lemonade);
     give_adult(vide2);
-
+    //FIXME
     let coffee = Some("coffee");
     //let _rien = None;
     let nothing = Some("chocolate");
 
     drink(coffee);
     drink(nothing);
-    drink(vide1);
+    drink(vide2);
+}
+
+use std::mem;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+// A Rectangle can be specified by where its top left and bottom right
+// corners are in space
+#[allow(dead_code)]
+struct Rectangle {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+fn origin() -> Point {
+    Point { x: 0.0, y: 0.0 }
+}
+
+fn boxed_origin() -> Box<Point> {
+    // Allocate this point on the heap, and return a pointer to it
+    Box::new(Point { x: 0.0, y: 0.0 })
+}
+#[allow(dead_code)]
+fn act1() {
+    // (all the type annotations are superfluous)
+    // Stack allocated variables
+    let point: Point = origin();
+    let rectangle: Rectangle = Rectangle {
+        top_left: origin(),
+        bottom_right: Point { x: 3.0, y: -4.0 }
+    };
+
+    // Heap allocated rectangle
+    let boxed_rectangle: Box<Rectangle> = Box::new(Rectangle {
+        top_left: origin(),
+        bottom_right: Point { x: 3.0, y: -4.0 },
+    });
+
+    // The output of functions can be boxed
+    let boxed_point: Box<Point> = Box::new(origin());
+
+    // Double indirection
+    let box_in_a_box: Box<Box<Point>> = Box::new(boxed_origin());
+
+    println!("Point occupies {} bytes on the stack",
+             mem::size_of_val(&point));
+    println!("Rectangle occupies {} bytes on the stack",
+             mem::size_of_val(&rectangle));
+
+    // box size == pointer size
+    println!("Boxed point occupies {} bytes on the stack",
+             mem::size_of_val(&boxed_point));
+    println!("Boxed rectangle occupies {} bytes on the stack",
+             mem::size_of_val(&boxed_rectangle));
+    println!("Boxed box occupies {} bytes on the stack",
+             mem::size_of_val(&box_in_a_box));
+
+    // Copy the data contained in `boxed_point` into `unboxed_point`
+    let unboxed_point: Point = *boxed_point;
+    println!("Unboxed point occupies {} bytes on the stack",
+             mem::size_of_val(&unboxed_point));
+}
+
+fn main() {
+    // You can use escapes to write bytes by their hexadecimal values...
+    let byte_escape = "I'm writing \x52\x75\x73\x74!";
+    let byte_escape2 ="\u{2115}\x30\u{2119}\u{211A}\x31\u{2124}\u{2102}\u{210d}\x3F";
+    let byte_simple ="\x3F";
+    println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+
+    // ...or Unicode code points.
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!("Unicode character {} (U+211D) is called {}",
+             unicode_codepoint, character_name );
+    println!("Simple string -> {} size {}",byte_simple,mem::size_of_val(byte_simple));
+    println!("New string -> >{}< size {}",byte_escape2,mem::size_of_val(byte_escape2));
+    println!("Old string -> {} size {}",unicode_codepoint,mem::size_of_val(unicode_codepoint));
+
+
+    let long_string = "String literals
+                        can span multiple lines.
+                        The linebreak and indentation here ->\
+                        <- can be escaped too!";
+    println!("{}", long_string);
+    for x in byte_escape2.chars() {
+        println!("> {}", x);
+    }
 }
